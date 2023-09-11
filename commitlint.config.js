30da2ca505f3e8972ced7c04d1faee30e6e032d9
이호연 <ho991217@kakao.com>
2023-09-11 11:58:23 +0900

    docs: 커밋 린트 적용

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,4 @@
 module.exports = {
    extends: ['@commitlint/config-conventional'],
-   plugins: [
-      {
-         rules: {
-            'develop-rule': ({ subject }) => {
-               const regex =
-                  /^(develop|feat|fix|hotfix|release|revert|style|test|docs|chore|build|ci|perf|refactor|wip)(\(.+\))?: .{1,50}/;
-               return [
-                  regex.test(subject),
-                  '다음 중 한가지 header를 포함해야합니다.\ndevelop|feat|fix|hotfix|release|revert|style|test|docs|chore|build|ci|perf|refactor|wip',
-               ];
-            },
-         },
-      },
-   ],
+   plugins: [],
 };
