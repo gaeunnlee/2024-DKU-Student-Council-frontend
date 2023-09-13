70fbea24c03d8454dc873b052dd1ee48f0b81b47
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-09-14 00:18:49 +0900

    feat: 회원가입 학생인증 페이지 구현 (#9)

---
+++ src/pages/signupVerify.tsx
@@ -0,0 +1,58 @@
+import React, { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../constants';
+import axios from 'axios';
+
+export default function SignupVerify() {
+   const navigate = useNavigate();
+
+   const [userInput, setUserInput] = useState({
+      studentId: '',
+      password: '',
+   });
+
+   const handle = {
+      signupVerify: async (e: FormEvent<HTMLFormElement>) => {
+         e.preventDefault();
+         try {
+            const response = await axios.post('http://localhost:5000/signup/verify', {
+               studentId: userInput.studentId,
+               password: userInput.password,
+            });
+            if (response.status === 200) {
+               alert(response.data.msg);
+               navigate(ROUTES.MAIN);
+            }
+         } catch (error) {
+            alert('인증에 실패하였습니다.');
+         }
+      },
+   };
+
+   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUserInput({
+         ...userInput,
+         [e.target.name]: e.target.value,
+      });
+   };
+
+   return (
+      <form onSubmit={handle.signupVerify}>
+         <input
+            type='text'
+            name='studentId'
+            placeholder='학번'
+            value={userInput.studentId}
+            onChange={handleInputChange}
+         />
+         <input
+            type='password'
+            name='password'
+            placeholder='비밀번호'
+            value={userInput.password}
+            onChange={handleInputChange}
+         />
+         <button>인증</button>
+      </form>
+   );
+}
