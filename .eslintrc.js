cdc1f27e138906ff70356c8cd94bd28fe0d2d000
gaeunnlee <gaeunnnleee@gmail.com>
2023-09-17 20:07:01 +0900

    chore: eslint linebreak-style windows 추가 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
    plugins: ['@typescript-eslint', 'react', 'cypress'],
    rules: {
       indent: ['error', 3],
-      'linebreak-style': ['error', 'unix'],
+      'linebreak-style': ['error', process.env.NODE_ENV === 'prod' ? 'unix' : 'windows'],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
    },
