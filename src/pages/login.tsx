4ecc9b0071def44851321f358994fff821b7f0bb
gaeunnlee <gaeunnnleee@gmail.com>
2023-09-17 20:15:42 +0900

    feat: 로그인 API 연결

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,30 +1,60 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../constants';
 import axios from 'axios';
 
+interface LoginProps {
+   studentId: string;
+   password: string;
+}
+
 export default function Login() {
    const navigate = useNavigate();
+   const [loginInfo, setLoginInfo] = useState<LoginProps>({
+      studentId: '',
+      password: '',
+   });
 
    const handle = {
       login: async (e: FormEvent<HTMLFormElement>) => {
          e.preventDefault();
          // 로그인 로직
-         const { data } = await axios.post('http://localhost:5000/login', {
-            id: '12345678',
-            password: 'qwer1234',
-         });
-
+         const { data } = await axios.post('/user/login', loginInfo);
          console.log(data);
+         alert('로그인되었습니다');
          navigate(ROUTES.MAIN);
       },
    };
 
    return (
-      <form data-testid='login-form' onSubmit={handle.login}>
-         <input data-testid='id-input' type='text' placeholder='학번' />
-         <input data-testid='password-input' type='password' placeholder='비밀번호' />
-         <button data-testid='login-button'>로그인</button>
-      </form>
+      <>
+         <form data-testid='login-form' onSubmit={handle.login}>
+            <input
+               value={loginInfo.studentId}
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  if (e.target.value.length < 9) {
+                     setLoginInfo((prev) => {
+                        return { ...prev, studentId: e.target.value };
+                     });
+                  }
+               }}
+               data-testid='id-input'
+               type='number'
+               placeholder='학번'
+            />
+            <input
+               value={loginInfo.password}
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setLoginInfo((prev) => {
+                     return { ...prev, password: e.target.value };
+                  });
+               }}
+               data-testid='password-input'
+               type='password'
+               placeholder='비밀번호'
+            />
+            <button data-testid='login-button'>로그인</button>
+         </form>
+      </>
    );
 }
