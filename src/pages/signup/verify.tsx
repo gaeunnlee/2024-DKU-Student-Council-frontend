6546f5741f09e70a4cd9fad1062ef2ecad110ee9
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-09-19 22:54:24 +0900

    feat: 학생 인증 api 연결 및 tailwind 적용 (#9)

--- src/pages/signup/verify.tsx
+++ src/pages/signup/verify.tsx
@@ -1,58 +1,95 @@
 import React, { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ROUTES } from '../../constants';
-import axios from 'axios';
+import { CONSTANTS, API_PATH, ROUTES } from '../../constants';
+import axios, { AxiosError } from 'axios';
+import { Regex } from '../../utils/regex';
+
+const { SERVER_URL } = CONSTANTS;
+
+export interface IverifyInfo {
+   studentId: string;
+   password: string;
+}
 
 export default function SignupVerify() {
    const navigate = useNavigate();
 
-   const [userInput, setUserInput] = useState({
+   const [verifyInfo, setVerifyInfo] = useState<IverifyInfo>({
       studentId: '',
       password: '',
    });
 
-   const handle = {
-      signupVerify: async (e: FormEvent<HTMLFormElement>) => {
-         e.preventDefault();
-         try {
-            const response = await axios.post('http://localhost:5000/signup/verify', {
-               studentId: userInput.studentId,
-               password: userInput.password,
-            });
-            if (response.status === 200) {
-               alert(response.data.msg);
-               navigate(ROUTES.MAIN);
-            }
-         } catch (error) {
-            alert('인증에 실패하였습니다.');
-         }
-      },
+   const [studentIdError, setStudentIdError] = useState<string | null>(null);
+   const [passwordError, setPasswordError] = useState<string | null>(null);
+
+   const verify = async (verifyInfo: IverifyInfo) => {
+      try {
+         const response = await axios.post(SERVER_URL + API_PATH.SIGNUP_VERIFY, verifyInfo);
+         navigate(ROUTES.MAIN); // 테스트차 성공 시 main으로 이동
+         console.log(response);
+      } catch (error) {
+         if (error instanceof AxiosError) return error.response;
+      }
    };
 
    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setUserInput({
-         ...userInput,
-         [e.target.name]: e.target.value,
+      const { name, value } = e.target;
+      setVerifyInfo({
+         ...verifyInfo,
+         [name]: value,
       });
+
+      // 유효성 검사
+      if (name === 'studentId') {
+         if (!Regex.studentId.test(value)) {
+            setStudentIdError('유효한 학번을 입력하세요 (8자리 숫자)');
+         } else {
+            setStudentIdError(null);
+         }
+      } else if (name === 'password') {
+         if (!Regex.password.test(value)) {
+            setPasswordError(
+               '비밀번호는 8자 이상 16자 이하이며, 영문 대소문자, 숫자, 특수문자 중 3가지 이상 조합되어야 합니다.',
+            );
+         } else {
+            setPasswordError(null);
+         }
+      }
+   };
+
+   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (studentIdError || passwordError) {
+         return;
+      }
+      verify(verifyInfo);
    };
 
    return (
-      <form onSubmit={handle.signupVerify}>
-         <input
-            type='text'
-            name='studentId'
-            placeholder='학번'
-            value={userInput.studentId}
-            onChange={handleInputChange}
-         />
-         <input
-            type='password'
-            name='password'
-            placeholder='비밀번호'
-            value={userInput.password}
-            onChange={handleInputChange}
-         />
-         <button>인증</button>
-      </form>
+      <div className='flex items-center justify-center min-h-screen'>
+         <form onSubmit={handleSubmit} className='max-w-md mx-auto'>
+            <input
+               type='number'
+               name='studentId'
+               placeholder='학번 입력'
+               value={verifyInfo.studentId}
+               onChange={handleInputChange}
+               className='w-full p-2 mb-4 rounded border bg-gray-100'
+            />
+            {studentIdError && <div className='text-red-500 mb-4'>{studentIdError}</div>}
+            <input
+               type='password'
+               name='password'
+               placeholder='비밀번호 입력'
+               value={verifyInfo.password}
+               onChange={handleInputChange}
+               className='w-full p-2 mb-4 rounded border bg-gray-100'
+            />
+            {passwordError && <div className='text-red-500 mb-4'>{passwordError}</div>}
+            <button type='submit' className='w-full p-2 bg-blue-500 text-white rounded'>
+               인증
+            </button>
+         </form>
+      </div>
    );
 }
