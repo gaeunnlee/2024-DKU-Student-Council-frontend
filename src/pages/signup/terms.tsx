8287b97f581015072fc6da2a950de01d3532dddc
9ye1 <jastu8646@gmail.com>
2023-09-22 19:29:42 +0900

    라우터 설정/ 전체동의 구현

--- src/pages/signup/terms.tsx
+++ src/pages/signup/terms.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from 'constant';
 
 export default function SignupTerms() {
-   const [agreeCheck, setAgreeCheck] = useState([false, false]);
-   //const navigate = useNavigate();
+   const [agreeCheck, setAgreeCheck] = useState([false, false, false]);
+   const navigate = useNavigate();
 
    const handleCheckboxChange = (index: number) => {
-      const newAgreeCheck = [...agreeCheck];
-      newAgreeCheck[index] = !newAgreeCheck[index];
-      setAgreeCheck(newAgreeCheck);
+      if (index === 3) {
+         if (agreeCheck[0] && agreeCheck[1] && agreeCheck[2]) {
+            const newAgreeCheck = [false, false, false];
+            setAgreeCheck(newAgreeCheck);
+         } else {
+            const newAgreeCheck = [true, true, true];
+            setAgreeCheck(newAgreeCheck);
+         }
+      } else {
+         const newAgreeCheck = [...agreeCheck];
+         newAgreeCheck[index] = !newAgreeCheck[index];
+         setAgreeCheck(newAgreeCheck);
+      }
    };
 
    const nextHandle = () => {
       // 체크 여부 확인
       if (agreeCheck[0] && agreeCheck[1] && agreeCheck[2]) {
-         // 회원가입 다음 화면으로
-         // navigate(ROUTE.);
+         navigate(ROUTES.SIGNUP.VERIFY);
       } else {
          alert('약관에 동의해야 회원가입이 가능합니다.');
       }
@@ -27,29 +37,30 @@ export default function SignupTerms() {
          <div>
             <div>
                <label>
+                  모두동의하기
                   <input
                      type='checkbox'
-                     name='agree1'
-                     checked={agreeCheck[0]}
-                     onChange={() => handleCheckboxChange(0)}
+                     checked={agreeCheck[0] && agreeCheck[1] && agreeCheck[2]}
+                     onChange={() => handleCheckboxChange(3)}
                   />
+               </label>
+               <p>
+                  단국대학교 총학생회 홈페이지는 단국대학교 홈페이지 서비스 회원가입 및 본인 인증을 위하여
+                  아래와 같이 개인정보를 수집, 이용합니다.
+               </p>
+               <label>
+                  <input type='checkbox' checked={agreeCheck[0]} onChange={() => handleCheckboxChange(0)} />
                   <span>[필수]</span>개인정보 수집, 이용 동의
                </label>
                <p>
                   단국대학교 총학생회 홈페이지는 단국대학교 홈페이지 서비스 회원가입 및 본인 인증을 위하여
-                  아래와 같이 개인정보를 수집, 이용합니다.{' '}
+                  아래와 같이 개인정보를 수집, 이용합니다.
                </p>
             </div>
 
             <div>
                <label>
-                  <input
-                     type='checkbox'
-                     id='2'
-                     name=''
-                     checked={agreeCheck[1]}
-                     onChange={() => handleCheckboxChange(1)}
-                  />
+                  <input type='checkbox' checked={agreeCheck[1]} onChange={() => handleCheckboxChange(1)} />
                   <span>[필수]</span>개인정보 제 3자 제공 동의
                </label>
                <p>
@@ -60,14 +71,7 @@ export default function SignupTerms() {
 
             <div>
                <label>
-                  <input
-                     type='checkbox'
-                     id='3'
-                     name='xxx'
-                     value='yyy'
-                     checked={agreeCheck[2]}
-                     onChange={() => handleCheckboxChange(2)}
-                  />
+                  <input type='checkbox' checked={agreeCheck[2]} onChange={() => handleCheckboxChange(2)} />
                   <span>[필수]</span>개인정보 수집, 이용 동의
                </label>
                <p>
