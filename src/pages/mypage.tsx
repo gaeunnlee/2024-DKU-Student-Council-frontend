94201e84cdc14a36e2c4f5ad343691f5c0b54dd7
이호연 <ho991217@kakao.com>
2023-09-22 15:56:59 +0900

    feat: 마이페이지 수정

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -5,6 +5,7 @@ import { useAlert } from 'hooks/useAlert';
 import { useEffectOnce } from 'hooks/useEffectOnce';
 import Box from 'components/ui/box';
 import Button from 'components/ui/button';
+import { TextSkeleton } from 'components/ui/skeleton';
 
 interface IMyInfo {
    studentId: string;
@@ -19,6 +20,10 @@ interface IMyInfo {
    likedPostCount: number;
    admin: boolean;
 }
+
+const MyPageSkeleton = ({ width }: { width: number }) => (
+   <TextSkeleton className={'h-[2rem] rounded-md'} style={{ width: width + 'rem' }} />
+);
 
 export default function MyPage() {
    const { alert } = useAlert();
@@ -43,7 +48,11 @@ export default function MyPage() {
          <div className='flex justify-between items-center'>
             <label className='text-xs'>
                닉네임
-               <h4 className='text-2xl font-medium'>{myInfo?.nickname}</h4>
+               {myInfo ? (
+                  <h4 className='text-2xl font-medium'>{myInfo?.nickname}</h4>
+               ) : (
+                  <MyPageSkeleton width={5} />
+               )}
             </label>
             <Box>
                <span>{myInfo?.admin ? '관리자' : '재학생'}</span>
@@ -51,32 +60,68 @@ export default function MyPage() {
          </div>
          <Box className='mt-4'>
             <label className='text-xs'>학번</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.studentId}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.studentId}</h4>
+            ) : (
+               <MyPageSkeleton width={7} />
+            )}
 
             <label className='text-xs'>이름</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.username}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.username}</h4>
+            ) : (
+               <MyPageSkeleton width={5} />
+            )}
 
             <label className='text-xs'>학과</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.department}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.department}</h4>
+            ) : (
+               <MyPageSkeleton width={8} />
+            )}
 
             <label className='text-xs'>전공</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.major}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.major}</h4>
+            ) : (
+               <MyPageSkeleton width={8} />
+            )}
 
             <label className='text-xs'>학번</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.yearOfAdmission}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.yearOfAdmission}</h4>
+            ) : (
+               <MyPageSkeleton width={4} />
+            )}
 
             <label className='text-xs'>전화번호</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.phoneNumber}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.phoneNumber}</h4>
+            ) : (
+               <MyPageSkeleton width={10} />
+            )}
          </Box>
          <Box className='mt-4'>
             <label className='text-xs'>작성한 게시글</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.writePostCount}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.writePostCount}</h4>
+            ) : (
+               <MyPageSkeleton width={3} />
+            )}
 
             <label className='text-xs'>댓글 단 게시글</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.commentedPostCount}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.commentedPostCount}</h4>
+            ) : (
+               <MyPageSkeleton width={4} />
+            )}
 
             <label className='text-xs'>좋아요 누른 게시글</label>
-            <h4 className='text-2xl font-medium'>{myInfo?.likedPostCount}</h4>
+            {myInfo ? (
+               <h4 className='text-2xl font-medium'>{myInfo?.likedPostCount}</h4>
+            ) : (
+               <MyPageSkeleton width={2} />
+            )}
          </Box>
          <Box className='mt-4 flex flex-col'>
             <Button>로그아웃</Button>
