80f5b32ddfe6c2645659e210183f85ae59f1d97f
gaeunnlee <gaeunnnleee@gmail.com>
2023-09-26 14:45:21 +0900

    feat: 청원게시판 API RESPONSE 세팅

--- src/pages/petition/index.tsx
+++ src/pages/petition/index.tsx
@@ -1,9 +1,26 @@
 import axios from 'axios';
 import { API_PATH } from 'constant';
 import { useEffectOnce } from 'hooks/useEffectOnce';
-import React from 'react';
+import React, { useState } from 'react';
+
+interface IPetitionPost {
+   id: string;
+   title: string;
+   author: string;
+   body: string;
+   createdAt: string;
+   files: string;
+   views: string;
+   tag: string;
+   status: string;
+   expiresAt: string;
+   agreeCount: string;
+   blinded: string;
+}
 
 export default function PetitionBoard() {
+   const [board, setBoard] = useState<IPetitionPost[]>();
+
    /* 청원게시판 글 목록 불러오기 */
    useEffectOnce(() => {
       fetchBoard();
@@ -11,8 +28,10 @@ export default function PetitionBoard() {
 
    const fetchBoard = () => {
       axios.get(API_PATH.POST.PETITON).then(({ data }) => {
-         console.log(data.content);
+         setBoard(data.content);
+         console.log(board);
       });
    };
+
    return <>청원게시판</>;
 }
