64ebae17252f5a1baf61fa9dd26bf9c5643ed6b3
gaeunnlee <gaeunnnleee@gmail.com>
2023-09-26 14:35:04 +0900

    feat: 청원게시판 API 연결

--- src/pages/petition/index.tsx
+++ src/pages/petition/index.tsx
@@ -1,7 +1,18 @@
+import axios from 'axios';
+import { API_PATH } from 'constant';
+import { useEffectOnce } from 'hooks/useEffectOnce';
 import React from 'react';
 
-
-
 export default function PetitionBoard() {
+   /* 청원게시판 글 목록 불러오기 */
+   useEffectOnce(() => {
+      fetchBoard();
+   });
+
+   const fetchBoard = () => {
+      axios.get(API_PATH.POST.PETITON).then(({ data }) => {
+         console.log(data.content);
+      });
+   };
    return <>청원게시판</>;
 }
