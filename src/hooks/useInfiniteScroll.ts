ba9becea40f3afba4103a4d8fcb61a40282cfa76
gaeunnlee <gaeunnnleee@gmail.com>
2023-10-12 16:07:06 +0900

    refactor: useInifiniteScroll 훅 리팩토링

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 export const useInfiniteScroll = <T>(api: string) => {
    const [list, setList] = useState<T[]>([]);
    const [page, setPage] = useState(0);
+   const [fetchSuccess, setFetchSuccess] = useState(false);
    const [isLoading, setIsLoading] = useState(true);
    const bottom = useRef<HTMLDivElement>(null);
    const callback = (entries: IntersectionObserverEntry[]) => {
@@ -20,16 +21,26 @@ export const useInfiniteScroll = <T>(api: string) => {
       return () => observer.disconnect();
    });
 
-   const fetchList = (boardPage: number) => {
-      setIsLoading(true);
-      axios.get(`${api}?page=${boardPage}&size=10&sort=id,desc`).then(({ data }) => {
+   const fetchList = async (boardPage: number) => {
+      setFetchSuccess(false);
+      try {
+         const { data } = await axios.get(`${api}?page=${boardPage}&size=10&sort=id,desc`);
          setList((prev) => {
             return page === 0 ? data.content : prev.concat(data.content);
          });
-         setIsLoading(data.content.length === 0);
-      });
+         setFetchSuccess(true);
+      } catch (error) {
+         alert(error);
+      }
    };
+
+   // fetchList 성공시 isLoading false
    useEffect(() => {
+      fetchSuccess && setIsLoading(list.length === 0);
+   }, [fetchSuccess]);
+
+   useEffect(() => {
+      setIsLoading(true);
       fetchList(page);
    }, [page]);
 
