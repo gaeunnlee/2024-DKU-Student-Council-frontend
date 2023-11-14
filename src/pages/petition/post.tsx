6ee895a8cea52e610b08fbfad774228c906add8a
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-11-14 17:53:18 +0900

    feat: 게시글 작성 form 훅 및 컴포넌트 추가 (#37)

--- src/pages/petition/post.tsx
+++ src/pages/petition/post.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Input from 'components/ui/input';
-import Button from 'components/ui/button';
-import { useApi } from 'hooks/useApi';
 import { API_PATH } from 'constant';
-import TextEditor from 'components/common/editor/index';
 import useImageUpload from 'hooks/useImageUpload';
-
-export interface IFormInfo {
-   title: string;
-   body: string;
-   files: File[];
-}
+import { IFormInfo } from 'hooks/useFormUpload';
+import { useFormUpload } from 'hooks/useFormUpload';
+import Post from 'components/main/post';
 
 export default function PetitionForm() {
    const initFormInfo: IFormInfo = {
@@ -18,67 +11,23 @@ export default function PetitionForm() {
       body: '',
       files: [],
    };
-   const [formInfo, setFormInfo] = React.useState<IFormInfo>(initFormInfo);
 
-   const handleUpdate = (value: string) => {
-      const cleanedValue = value.replaceAll(/<\/?p[^>]*>/g, '').replace(/<br>/g, '');
-      setFormInfo({
-         ...formInfo,
-         body: cleanedValue,
-      });
-   };
+   const { formInfo, setFormInfo, handleUpdate, handleSubmit } = useFormUpload(
+      initFormInfo,
+      API_PATH.POST.PETITION,
+   );
 
    const { imageUrls, addImage, deleteImage } = useImageUpload();
-   const { post } = useApi();
-
-   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const formData = new FormData();
-      formData.append('title', formInfo.title);
-      formData.append('body', formInfo.body);
-      for (const file of formInfo.files) {
-         formData.append('files', file);
-      }
-      post<IFormInfo, number>(API_PATH.POST.PETITION, formInfo, {
-         authenticate: true,
-         contentType: 'multipart/form-data',
-         log: true,
-      });
-   };
 
    return (
-      <form onSubmit={handleSubmit} encType='multipart/form-data' className='flex-col'>
-         <Input
-            label='제목'
-            type='text'
-            id='title'
-            name='title'
-            value={formInfo.title}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-               setFormInfo((prev) => {
-                  return { ...prev, title: e.target.value };
-               });
-            }}
-         />
-         <TextEditor value={!formInfo.body ? '<br>' : `<p>${formInfo.body}</p>`} onChange={handleUpdate} />
-         <label htmlFor='input-file'>
-            사진추가
-            <Input
-               type='file'
-               id='input-file'
-               multiple
-               onChange={(e) => addImage({ e, formInfo, setFormInfo })}
-            />
-         </label>
-         {imageUrls.map((imageUrl, id) => (
-            <div key={id}>
-               <Button onClick={() => deleteImage({ id, setFormInfo })} className='w-2 h-4'>
-                  x
-               </Button>
-               <img src={imageUrl} alt={`Image-${id}`} className='w-10 h-10' />
-            </div>
-         ))}
-         <Button type='submit'>업로드</Button>
-      </form>
+      <Post
+         formInfo={formInfo}
+         setFormInfo={setFormInfo}
+         handleUpdate={handleUpdate}
+         handleSubmit={handleSubmit}
+         imageUrls={imageUrls}
+         addImage={addImage}
+         deleteImage={deleteImage}
+      />
    );
 }
