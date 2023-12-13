1f12ebf9bb35157bad0ade090c64062b625a5d59
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-13 18:45:24 +0900

    feat: 총학생회 오시는길 페이지 구현

---
+++ src/pages/council/location.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useEffectOnce } from 'hooks/useEffectOnce';
+import { useLayout } from 'hooks/useLayout';
+import map from '../../assets/images/map.png';
+
+export default function Location() {
+   const { setFullscreen, setTitle, setHeading, setSubHeading, setBackButton } = useLayout();
+
+   useEffectOnce(() => {
+      setFullscreen(false);
+      setTitle('총학생회');
+      setHeading('총학생회');
+      setSubHeading('오시는 길');
+      setBackButton(true);
+   });
+
+   return (
+      <>
+         <img className='w-[366px] h-[240px] mx-auto' src={map} />
+         <ul>
+            <li>
+               <h2 className='font-semibold'>위치</h2>
+               <span>혜당관 406호 총학생회실</span>
+            </li>
+            <li>
+               <h2 className='font-semibold'>주소</h2>
+               <span>(16890) 경기도 용인시 수지구 죽전동 1491 단국대학교 혜당관 406호 총학생회실</span>
+            </li>
+            <li>
+               <h2 className='font-semibold'>전화</h2>
+               <span>{'031)'}8005-2680-1</span>
+            </li>
+            <li>
+               <h2 className='font-semibold'>이메일</h2>
+               <span>dkudamda@gmail.com</span>
+            </li>
+            <li>
+               <h2 className='font-semibold'>인스타그램</h2>
+               <span>@dku_damda</span>
+            </li>
+         </ul>
+      </>
+   );
+}
