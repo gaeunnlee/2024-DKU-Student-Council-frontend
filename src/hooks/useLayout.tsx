269426b6c4596c7293f2556f35373189ebe14f0c
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-13 18:53:28 +0900

    feat: gnhState 추가

--- src/hooks/useLayout.tsx
+++ src/hooks/useLayout.tsx
@@ -1,15 +1,19 @@
 import { useEffectOnce } from './useEffectOnce';
 import { gnbState } from 'stores/gnb-store';
+import { gnhState } from 'stores/gnh-store';
 import { navStore } from 'stores/nav-store';
 
 export const useLayout = () => {
    const { setTitle, setBackButton } = gnbState();
    const { setFullscreen } = navStore();
+   const { setHeading, setSubHeading } = gnhState();
 
    useEffectOnce(() => {
       return () => {
          setTitle(null);
          setBackButton(false);
+         setHeading(null);
+         setSubHeading(null);
          setFullscreen(false);
       };
    });
@@ -17,6 +21,8 @@ export const useLayout = () => {
    return {
       setTitle,
       setBackButton,
+      setHeading,
+      setSubHeading,
       setFullscreen,
    };
 };
