2b38608560da1c3951f0f942201e739119c17ef8
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-19 01:02:22 +0900

    refactor: 전체 레이아웃 세팅 함수 setLayout 추가

--- src/hooks/useLayout.tsx
+++ src/hooks/useLayout.tsx
@@ -3,6 +3,14 @@ import { gnbState } from 'stores/gnb-store';
 import { gnhState } from 'stores/gnh-store';
 import { navStore } from 'stores/nav-store';
 
+interface LayoutProps {
+   title: string | null;
+   backButton: boolean;
+   heading?: string | null;
+   subHeading?: string | null;
+   fullscreen: boolean;
+}
+
 export const useLayout = () => {
    const { setTitle, setBackButton } = gnbState();
    const { setFullscreen } = navStore();
@@ -10,19 +18,23 @@ export const useLayout = () => {
 
    useEffectOnce(() => {
       return () => {
-         setTitle(null);
-         setBackButton(false);
-         setHeading(null);
-         setSubHeading(null);
-         setFullscreen(false);
+         setLayout({
+            title: null,
+            backButton: true,
+            heading: null,
+            subHeading: null,
+            fullscreen: false,
+         });
       };
    });
 
-   return {
-      setTitle,
-      setBackButton,
-      setHeading,
-      setSubHeading,
-      setFullscreen,
+   const setLayout = ({ title, backButton, heading, subHeading, fullscreen }: LayoutProps) => {
+      title !== undefined && setTitle(title);
+      setBackButton(backButton);
+      heading !== undefined && setHeading(heading);
+      subHeading !== undefined && setSubHeading(subHeading);
+      fullscreen !== undefined && setFullscreen(fullscreen);
    };
+
+   return { setLayout };
 };
