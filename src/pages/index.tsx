2461724761bfae271b2764a571d98c75d00eb3e8
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-20 02:30:28 +0900

    feat: 학식 정보 컴포넌트 추가 및 api 타입, 통신 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { API_PATH } from 'constant';
 import { useEffectOnce } from 'hooks/useEffectOnce';
-import { Banner, Notice, Petition } from 'components/main';
+import { Banner, Notice, Petition, Cafeteria } from 'components/main';
 import type { IBanner } from 'components/main/banner';
 import type { INotice } from 'components/main/notice';
 import type { IPetition } from 'components/main/petition';
 import { useApi } from 'hooks/useApi';
+import { useLayout } from 'hooks/useLayout';
+import Service from 'components/main/service';
 
 interface IMain {
    carousels: IBanner[];
-   recentNews: INotice[];
+   recentNotices: INotice[];
    popularPetitions: IPetition[];
    recentConferences: [
       {
@@ -19,29 +21,54 @@ interface IMain {
    ];
 }
 
+export interface ICafeteria {
+   mealData: string;
+   breakfast: string;
+   lunch: string;
+   dinner: string;
+   other: string;
+}
+
 export default function Main() {
    const [main, setMain] = useState<IMain | null>(null);
+   const [cafeteria, setCafeteria] = useState<ICafeteria | null>(null);
    const { get } = useApi();
+   const { setLayout } = useLayout();
 
    const fetchMain = async () => {
       const data = await get<IMain>(API_PATH.MAIN.ROOT);
       setMain(data);
    };
 
+   const fetchMeal = async () => {
+      const data = await get<ICafeteria | null>(API_PATH.MAIN.CAFETERIA);
+      setCafeteria(data);
+   };
+
+   console.log(main);
+
    useEffectOnce(() => {
       fetchMain();
+      fetchMeal();
+      setLayout({
+         title: null,
+         backButton: false,
+         isMain: true,
+         fullscreen: false,
+         heading: 'DANKOOK UNIVERSITY',
+         subHeading: 'DANKOOK UNIV STUDENT COUNCIL',
+      });
    });
 
    return (
-      <main className='rounded-t-xl'>
-         <div className='px-6 pt-5 pb-4'>
-            <h1 className='text-4xl font-bold'>Dankook University</h1>
-            <h2 className='text-2xl font-medium mb-2'>도전하는 지성, 세계를 향한 창조</h2>
-            <span className='text-xs'>DANKOOK UNIVERSITY STUDENT COUNCIL</span>
-         </div>
+      <main>
          <Banner banners={main?.carousels} />
-         <Notice notices={main?.recentNews} />
-         <Petition petitions={main?.popularPetitions} />
+         <div className='bg-gray-100 pt-5 pb-4'>
+            <Notice notices={main?.recentNotices} />
+            <Petition petitions={main?.popularPetitions} />
+            <Cafeteria cafeteria={cafeteria || undefined} />
+            <Service />
+         </div>
       </main>
    );
 }
