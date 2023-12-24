943c69a41f5c9876efd89f42bf7e300a92703810
gaeunnlee <gaeunnnleee@gmail.com>
2023-12-25 02:50:05 +0900

    chore: 페이지별 Board Style 적용

--- src/layouts/BoardLayout.tsx
+++ src/layouts/BoardLayout.tsx
@@ -2,7 +2,7 @@ import Board from 'components/common/board';
 import Text from 'components/ui/text';
 import { useInfiniteScroll } from 'hooks/useInfiniteScroll';
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface ICell {
    id: string;
@@ -23,28 +23,45 @@ export default function BoardLayout({ api }: { api: string }) {
    const { list, isLoading, bottom } = useInfiniteScroll<ICell>(api);
    const [isEmpty, setIsEmpty] = React.useState(false);
    const navigate = useNavigate();
+   const location = useLocation();
 
    useEffect(() => {
       setIsEmpty(list && list.length === 0);
    }, [list]);
+
    return (
       <>
          <Board>
             {isEmpty ? (
                <Board.NoData />
             ) : (
-               list?.map(({ id, status, title, createdAt, agreeCount, expiresAt }) => (
+               list?.map(({ id, status, title, createdAt, expiresAt }) => (
                   <Board.Cell
                      key={id}
                      onClick={() => {
                         navigate(`${id}`);
                      }}
                   >
-                     {status && <Text length={4}>{status}</Text>}
-                     <Text length={4}>{title}</Text>
-                     <Text length={4}>{createdAt.slice(0, 10).replaceAll('-', '.')}</Text>
-                     {agreeCount && <Text length={4}>{agreeCount}</Text>}
-                     {expiresAt && <Text length={4}>{expiresAt}</Text>}
+                     {/* 공지 */}
+                     {location.pathname === '/notice' && (
+                        <>
+                           <Text className='text-xl font-bold' length={4}>
+                              {title}
+                           </Text>
+                           <Text className='text-gray-400' length={4}>
+                              {createdAt.slice(0, 10).replaceAll('-', '.')}
+                           </Text>
+                        </>
+                     )}
+
+                     {/* 청원 */}
+                     {location.pathname === '/petition' && (
+                        <div className='flex justify-between leading-9 px-8'>
+                           <Text length={4}>{status}</Text>
+                           <Text length={4}>{title}</Text>
+                           <Text length={4}>{expiresAt}</Text>
+                        </div>
+                     )}
                   </Board.Cell>
                ))
             )}
