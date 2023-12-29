6a1b71785687824148380673a77ca60845c22267
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-29 17:11:18 +0900

    style: Gnh 메인 여부에 따른 스타일 변경

--- src/components/common/gnh/index.tsx
+++ src/components/common/gnh/index.tsx
@@ -10,12 +10,18 @@ const Gnh = ({ heading, subHeading, isMain }: GnhProps) => (
    <>
       <div className={'w-[390px] mx-auto px-8 py-8 justify-between h-[160px] items-center bg-black'}>
          {heading && (
-            <h2 className={`${isMain && 'text-center'} text-2xl font-semibold mb-4 text-white`}>{heading}</h2>
+            <h2
+               className={`${
+                  isMain ? 'text-center mb-1 font-black' : 'mb-4'
+               } text-2xl font-extrabold text-white`}
+            >
+               {heading}
+            </h2>
          )}
          {subHeading && (
             <h3
                className={`${
-                  isMain ? 'text-base text-center font-normal' : 'text-xl font-semibold'
+                  isMain ? 'text-xs text-center font-normal' : 'text-xl font-semibold'
                } text-white`}
             >
                {subHeading}
