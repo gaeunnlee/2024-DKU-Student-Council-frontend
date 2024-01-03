8558840098fb73d9fc36d031b4d646be5a683815
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2024-01-03 23:45:24 +0900

    style: 아이콘 타입 변경  스타일 코드 수정

--- src/components/common/nav/index.tsx
+++ src/components/common/nav/index.tsx
@@ -1,25 +1,25 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import { ReactComponent as MenuIcon } from '../../../assets/icons/menu.svg';
 import { ReactComponent as WithDankookIcon } from '../../../assets/icons/with_dankook.svg';
 import { ReactComponent as HomeIcon } from '../../../assets/icons/home.svg';
 import { ReactComponent as PartnershipIcon } from '../../../assets/icons/partnership.svg';
 import { ReactComponent as MyPageIcon } from '../../../assets/icons/mypage.svg';
-import { motion } from 'framer-motion';
-import { CONSTANTS, ROUTES } from 'constant';
+import { CONSTANTS } from 'constant';
 
 interface NavItem {
    id: number;
    name: string;
    path: string;
-   icon: ReactElement;
+   icon: React.ReactElement;
 }
 
 const navItems: NavItem[] = [
    {
       id: 1,
       name: 'Menu',
-      path: '/category',
+      path: '/menu',
       icon: <MenuIcon />,
    },
    {
@@ -31,44 +31,45 @@ const navItems: NavItem[] = [
    {
       id: 3,
       name: 'Home',
-      path: ROUTES.MAIN,
+      path: '/home',
       icon: <HomeIcon />,
    },
    {
       id: 4,
       name: '제휴사업',
-      path: ROUTES.RENTAL.ROOT,
+      path: '/partnership',
       icon: <PartnershipIcon />,
    },
    {
       id: 5,
       name: 'My page',
-      path: ROUTES.MYPAGE,
+      path: '/mypage',
       icon: <MyPageIcon />,
    },
 ];
 
 export default function Nav() {
    return (
-      // pwa인 경우: pb-[20px] h-[80px]
-      <motion.nav
-         className='w-[390px] bg-black left-[50%] translate-x-[-50%] fixed flex justify-between px-6 items-center max-w-3xl'
-         style={{ height: CONSTANTS.bottomNavSize }}
-         initial={{ bottom: '-60px' }}
-         animate={{ bottom: '0px' }}
-         exit={{ bottom: '-60px' }}
-         transition={{ duration: 0.3 }}
-      >
-         {navItems.map((el) => (
-            <Link
-               key={el.id}
-               to={el.path}
-               className='text-white text-xs font-normal flex flex-col items-center gap-1'
-            >
-               {el.icon}
-               {el.name}
-            </Link>
-         ))}
-      </motion.nav>
+      <>
+         <motion.nav
+            className='w-[390px] bg-black left-[50%] translate-x-[-50%] fixed flex justify-between px-6 items-center max-w-3xl'
+            style={{ height: CONSTANTS.bottomNavSize }}
+            initial={{ bottom: '-60px' }}
+            animate={{ bottom: '0px' }}
+            exit={{ bottom: '-60px' }}
+            transition={{ duration: 0.3 }}
+         >
+            {navItems.map((el) => (
+               <Link
+                  key={el.id}
+                  to={el.path}
+                  className='text-white text-[10px] font-normal flex flex-col justify-end items-center gap-1'
+               >
+                  {el.icon}
+                  {el.name}
+               </Link>
+            ))}
+         </motion.nav>
+      </>
    );
 }
