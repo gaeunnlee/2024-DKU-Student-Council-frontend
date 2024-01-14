426161d17cbc40ecb76be7231b7547ea6ca458ed
gaeunnlee <gaeunnnleee@gmail.com>
2024-01-15 05:34:14 +0900

    feat: 회칙 목록 BoardLayout 적용

--- src/pages/rule/index.tsx
+++ src/pages/rule/index.tsx
@@ -1,5 +1,22 @@
+import { API_PATH, CONSTANTS } from 'constant';
+import BoardLayout, { IBoardList } from 'layouts/BoardLayout';
 import React from 'react';
 
 export default function RuleBoard() {
-   return <></>;
+   const setCell = (data: IBoardList) => (
+      <a href={data.files[0].url} target='_blank' rel='noopener noreferrer' className='flex gap-2'>
+         <Title>{data.title}</Title>
+         <Date>{data.createdAt}</Date>
+      </a>
+   );
+   return (
+      <BoardLayout api={CONSTANTS.SERVER_URL + API_PATH.POST.RULE.ROOT} setCell={setCell} isLink={false} />
+   );
 }
+
+const Title = ({ children }: { children: React.ReactNode }) => (
+   <div className='grow text-center truncate'>{children}</div>
+);
+const Date = ({ children }: { children: React.ReactNode }) => (
+   <div>{String(children).slice(0, 10).replaceAll('-', '.')}</div>
+);
