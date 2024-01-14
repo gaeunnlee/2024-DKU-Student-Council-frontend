7782e55b9b5d92e08c38ce9d04ae69fc81a13b07
gaeunnlee <gaeunnnleee@gmail.com>
2024-01-14 22:36:08 +0900

    feat: 회의록 페이지 BoardLayout 적용

--- src/pages/conference/index.tsx
+++ src/pages/conference/index.tsx
@@ -1,5 +1,13 @@
+import { API_PATH, CONSTANTS } from 'constant';
+import BoardLayout, { IBoardList } from 'layouts/BoardLayout';
 import React from 'react';
 
 export default function ConferenceBoard() {
-   return <></>;
+   const setCell = (data: IBoardList) => (
+      <>
+         {data.title}
+         {data.createdAt}
+      </>
+   );
+   return <BoardLayout api={CONSTANTS.SERVER_URL + API_PATH.POST.CONFERENCE.ROOT} setCell={setCell} />;
 }
