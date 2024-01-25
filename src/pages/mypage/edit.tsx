706a5153d99efdb17551b7397ef93a50cc28df1f
gaeunnlee <gaeunnnleee@gmail.com>
2024-01-26 04:55:56 +0900

    refactor: 회원정보 수정 페이지 컴포넌트 분리

--- src/pages/mypage/edit.tsx
+++ src/pages/mypage/edit.tsx
@@ -4,11 +4,17 @@ import { useEffectOnce } from 'hooks/useEffectOnce';
 import { useLayout } from 'hooks/useLayout';
 import { useApi } from 'hooks/useApi';
 import { ProfileImage } from 'layouts/MyPageLayout';
-import { shadowStyle } from 'constants/style';
-import { FaCheckCircle } from 'react-icons/fa';
-import { FaCircleExclamation } from 'react-icons/fa6';
 import { IFormInfo, IMyInfo } from 'interfaces/mypage/edit';
 import { defaultFormInfo } from 'data/mypage/edit/defaultFormInfo';
+import {
+   Box,
+   InputBox,
+   InputButton,
+   InputText,
+   Label,
+   Message,
+   ValidationIcon,
+} from 'components/mypage/edit';
 
 export default function MyPageEdit() {
    const { setLayout } = useLayout();
@@ -95,57 +101,3 @@ export default function MyPageEdit() {
       </>
    );
 }
-
-const Box = ({ children, key }: { children: React.ReactNode; key: number }) => (
-   <div key={key} className='w-full rounded-lg bg-[#f4f4f4] p-4 flex flex-col gap-5'>
-      {children}
-   </div>
-);
-const Label = ({ text }: { text: string }) => <strong>{text}</strong>;
-const InputBox = ({ children }: { children: React.ReactNode }) => (
-   <div
-      className={`bg-white ${shadowStyle.default} rounded-lg outline-none w-full h-10 w-full flex justify-between items-center overflow-hidden mt-2`}
-   >
-      {children}
-   </div>
-);
-const InputText = ({
-   placeholder,
-   type,
-   value,
-   disabled,
-}: {
-   placeholder?: string;
-   type: string;
-   value?: string;
-   disabled: boolean;
-}) => (
-   <input
-      className={`outline-none h-10 border-none p-2 w-[250px] text-[0.9rem] ${
-         type === 'number' &&
-         '[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
-      }`}
-      type={type}
-      value={value}
-      disabled={disabled}
-      placeholder={placeholder}
-   />
-);
-const ValidationIcon = ({ validation }: { validation: null | boolean }) => (
-   <div className='mr-2'>
-      {validation !== null &&
-         (validation ? <FaCheckCircle color='green' /> : <FaCircleExclamation color='#c73a4a' />)}
-   </div>
-);
-const InputButton = ({ text, type, style }: { text: string; type?: string; style?: string }) => (
-   <button
-      className={`bg-black text-white rounded-lg px-3 h-full ${
-         type === 'big' && 'w-full h-8 py-1 mt-3'
-      } ${style}`}
-   >
-      {text}
-   </button>
-);
-const Message = ({ text }: { text: string }) => (
-   <p className='text-[#626262] text-[0.6rem] mt-1 px-3 break-words'>{text}</p>
-);
