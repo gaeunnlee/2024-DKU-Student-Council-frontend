df82805263ec0bbb699300283f9560eeb2395406
gaeunnlee <gaeunnnleee@gmail.com>
2024-02-06 12:59:34 +0900

    feat: 회원정보 갱신 API 연결

--- src/pages/mypage/update.tsx
+++ src/pages/mypage/update.tsx
@@ -1,19 +1,24 @@
-import Box from 'components/ui/box';
 import Button from 'components/ui/button';
 import Input from 'components/ui/input';
 import Text, { Title } from 'components/ui/text';
+import { API_PATH, CONSTANTS } from 'constants/api';
+import { useAlert } from 'hooks/useAlert';
+import { useApi } from 'hooks/useApi';
 import { useEffectOnce } from 'hooks/useEffectOnce';
-import { useFetchMyInfo } from 'hooks/useFetchMyInfo';
 import { useLayout } from 'hooks/useLayout';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { GoDotFill } from 'react-icons/go';
+import { useNavigate } from 'react-router-dom';
+import { checkInputRegex } from 'utils/checkInputRegex';
 
 export default function MyPageUpdate() {
    const { setLayout } = useLayout();
-   const { myInfo, fetchMyInfo } = useFetchMyInfo();
+   const [loginInfo, setLoginInfo] = useState({ dkuStudentId: '', dkuPassword: '' });
+   const { patch } = useApi();
+   const { alert } = useAlert();
+   const navigate = useNavigate();
 
    useEffectOnce(() => {
-      fetchMyInfo();
       setLayout({
          title: '',
          backButton: true,
@@ -26,19 +31,55 @@ export default function MyPageUpdate() {
       });
    });
 
-   useEffect(() => {
-      console.log(myInfo);
-   }, [myInfo]);
+   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (loginInfo.dkuStudentId.length > 0 && loginInfo.dkuPassword.length > 0) {
+         try {
+            await patch(`${CONSTANTS.SERVER_URL}${API_PATH.USER.DKU}`, loginInfo, { authenticate: true });
+            alert('인증되었습니다.');
+            navigate('/mypage');
+         } catch (error) {
+            alert(error);
+         }
+      } else {
+         alert('모두 입력해주세요.');
+      }
+   };
+
    return (
-      <Box className='border-none mt-10 flex flex-col gap-3 px-6'>
+      <form
+         className='border-none mt-10 flex flex-col gap-3 px-6'
+         onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+            handleSubmit(e);
+         }}
+      >
          <Title className='text-2xl font-bold'>회원 정보 갱신</Title>
          <Text length={4} className='flex text-sm items-center'>
             <GoDotFill />
             학생 인증
          </Text>
          <div>
-            <Input placeholder='학번 입력' className='w-full rounded-lg' />
-            <Input placeholder='비밀번호 입력' type='password' className='w-full rounded-lg' />
+            <Input
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setLoginInfo((prev) => ({
+                     ...prev,
+                     dkuStudentId: checkInputRegex(e.target.value, 'number') ?? '',
+                  }));
+               }}
+               maxLength={8}
+               placeholder='학번 입력'
+               className='w-full rounded-lg'
+               value={loginInfo.dkuStudentId}
+            />
+            <Input
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setLoginInfo((prev) => ({ ...prev, dkuPassword: e.target.value }));
+               }}
+               placeholder='비밀번호 입력'
+               type='password'
+               className='w-full rounded-lg'
+               value={loginInfo.dkuPassword}
+            />
          </div>
          <Text length={4} className='flex text-sm gap-1 text-neutral-400'>
             <GoDotFill size={30} />
@@ -48,6 +89,6 @@ export default function MyPageUpdate() {
          <Button variant='black' className='w-full'>
             인증
          </Button>
-      </Box>
+      </form>
    );
 }
