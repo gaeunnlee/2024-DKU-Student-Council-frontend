b66c89ec6df2677a0aff7e35527f76a574433323
sy-paik <sypaik.dev@gmail.com>
2024-02-26 18:22:26 +0900

    refactor: 버튼 컴포넌트 variant props 추가

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -3,14 +3,16 @@ import { Button as ChakraButton } from '@chakra-ui/react';
 import { ButtonProps as ChakraButtonProps } from '@chakra-ui/react';
 
 interface ButtonProps extends ChakraButtonProps {
+   variant?: 'default' | 'naked';
    children: React.ReactNode;
    className?: string;
    onClick?: () => void;
 }
 
-export default function Button({ className, children, onClick }: ButtonProps) {
+export default function Button({ variant, className, children, onClick }: ButtonProps) {
+   const buttonVariant = variant === 'default' ? 'bg-black' : 'bg-gray01';
    return (
-      <ChakraButton className={`bg-black text-white ${className}`} onClick={onClick}>
+      <ChakraButton className={`bg-black text-white ${className} ${buttonVariant}`} onClick={onClick}>
          {children}
       </ChakraButton>
    );
