b54a659c8df4d260acf014a743f064f47d9ed121
sy-paik <sypaik.dev@gmail.com>
2024-02-28 07:52:33 +0900

    chore: react query 환경 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './Router';
 import ModalProvider from 'components/ui/modal/modal-provider';
 import ChakraUIProvider from 'components/common/chakra';
 
+const queryClient = new QueryClient();
+
 export default function App() {
    return (
-      <ModalProvider>
-         <ChakraUIProvider>
-            <Router />
-         </ChakraUIProvider>
-      </ModalProvider>
+      <QueryClientProvider client={queryClient}>
+         <ModalProvider>
+            <ChakraUIProvider>
+               <Router />
+            </ChakraUIProvider>
+         </ModalProvider>
+      </QueryClientProvider>
    );
 }
