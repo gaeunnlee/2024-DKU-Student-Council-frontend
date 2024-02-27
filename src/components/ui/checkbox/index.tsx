c26961383484634335d99f5dfa91ba20a8851433
sy-paik <sypaik.dev@gmail.com>
2024-02-28 08:09:57 +0900

    feat: 체크박스 컴포넌트 구현

--- src/components/ui/checkbox/index.tsx
+++ src/components/ui/checkbox/index.tsx
@@ -2,18 +2,8 @@ import { Checkbox as ChakraCheckbox } from '@chakra-ui/react';
 import { CheckboxProps as ChakraCheckboxProps } from '@chakra-ui/react';
 import React from 'react';
 
-interface CheckboxProps extends ChakraCheckboxProps {
-   className?: string;
-}
+interface CheckboxProps extends ChakraCheckboxProps {}
 
-export default function Checkbox({ className, ...props }: CheckboxProps) {
-   return (
-      <ChakraCheckbox
-         spacing='1rem'
-         size={'lg'}
-         colorScheme='red'
-         className={className}
-         {...props}
-      ></ChakraCheckbox>
-   );
+export default function Checkbox({ ...props }: CheckboxProps) {
+   return <ChakraCheckbox size='md' {...props} className='!flex items-center' />;
 }
