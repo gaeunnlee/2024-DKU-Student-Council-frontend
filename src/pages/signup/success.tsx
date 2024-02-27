79089ca5a73acfe31916e3645eb527c820088201
sy-paik <sypaik.dev@gmail.com>
2024-02-28 08:40:21 +0900

    chore: 로그인 페이지로 이동하는 버튼 추가

--- src/pages/signup/success.tsx
+++ src/pages/signup/success.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import SvgIcon from 'components/common/icon/SvgIcon';
 import { useEffectOnce } from 'hooks/useEffectOnce';
 import { useLayout } from 'hooks/useLayout';
+import Button from 'components/ui/button';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from 'constants/route';
 
 export default function SignupSuccess() {
    const { setLayout } = useLayout();
+   const navigate = useNavigate();
+
+   const handleGoLogin = () => {
+      navigate(ROUTES.LOGIN);
+   };
 
    useEffectOnce(() => {
       setLayout({
@@ -12,11 +20,6 @@ export default function SignupSuccess() {
          backButton: true,
          isMain: false,
          fullscreen: true,
-         heading: '',
-         subHeading: '',
-         headingStyle: '',
-         headingText: '',
-         subHeadingText: '',
          margin: 'mt-[140px]',
          rounded: true,
       });
@@ -26,7 +29,10 @@ export default function SignupSuccess() {
          <div className='flex flex-col items-center justify-center gap-3'>
             <SvgIcon id='success' width={38} height={38} />
             <h2 className='font-bold text-xl'>회원가입 완료</h2>
-            <p className='text-xl'>회원가입이 완료되었습니다.</p>
+            <p className='text-xl mb-4'>회원가입이 완료되었습니다.</p>
+            <Button variant='default' size='md' borderRadius='20px' onClick={handleGoLogin}>
+               로그인하러 가기
+            </Button>
          </div>
       </div>
    );
