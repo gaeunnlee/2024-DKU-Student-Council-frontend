ba0835236f5112b4e17393797d8a1d067734bb65
sy-paik <sypaik.dev@gmail.com>
2024-02-28 08:06:34 +0900

    refactor: 각 style 객체 분리

---
+++ src/constants/textStyle.ts
@@ -0,0 +1,14 @@
+export const TextStyle = {
+   h1: {
+      fontSize: '24px',
+      fontWeight: 800,
+   },
+   h2: {
+      fontSize: '16px',
+      fontWeight: 700,
+   },
+   h3: {
+      fontSize: '14px',
+      fontWeight: 600,
+   },
+} as const;
