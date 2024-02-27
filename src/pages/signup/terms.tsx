8cad68bbf33713ed44d5f5e62404f9c31bbe18a9
sy-paik <sypaik.dev@gmail.com>
2024-02-28 08:37:23 +0900

    chore: 회원가입 동의항목 및 학생인증 form 컴포넌트 분리 적용

--- src/pages/signup/terms.tsx
+++ src/pages/signup/terms.tsx
@@ -1,34 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ROUTES } from 'constants/route';
-import { useAlert } from '../../hooks/useAlert';
-import Checkbox from 'components/ui/checkbox';
+import React from 'react';
 import { useEffectOnce } from 'hooks/useEffectOnce';
 import { useLayout } from 'hooks/useLayout';
-import Button from 'components/ui/button';
 import Heading from 'components/ui/typo/heading';
-import Box from 'components/ui/box';
-import Text from 'components/ui/typo/text';
+import Term from 'components/signup/term';
 
 export default function SignupTerms() {
-   const { alert } = useAlert();
    const { setLayout } = useLayout();
-
-   const [agreeCheck, setAgreeCheck] = useState([false, false, false]);
-   const navigate = useNavigate();
-
-   const onCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const name = e.target.name;
-      const newAgreeCheck = [...agreeCheck];
-      if (name === 'agree1') {
-         newAgreeCheck[0] = !newAgreeCheck[0];
-      } else if (name === 'agree2') {
-         newAgreeCheck[1] = !newAgreeCheck[1];
-      } else if (name === 'agree3') {
-         newAgreeCheck[2] = !newAgreeCheck[2];
-      }
-      setAgreeCheck(newAgreeCheck);
-   };
 
    useEffectOnce(() => {
       setLayout({
@@ -42,79 +19,18 @@ export default function SignupTerms() {
       });
    });
 
-   const onAllCheckboxChange = () => {
-      if (agreeCheck[0] && agreeCheck[1] && agreeCheck[2]) {
-         const newAgreeCheck = [false, false, false];
-         setAgreeCheck(newAgreeCheck);
-      } else {
-         const newAgreeCheck = [true, true, true];
-         setAgreeCheck(newAgreeCheck);
-      }
-   };
-
-   const goNext = () => {
-      // 체크 여부 확인
-      if (agreeCheck[0] && agreeCheck[1] && agreeCheck[2]) {
-         navigate(ROUTES.SIGNUP.VERIFY);
-      } else {
-         alert('약관에 동의해야 회원가입이 가능합니다.');
-      }
-   };
-
    return (
       <div className='px-10'>
-         <Heading as='h1' className='text-2xl font-extrabold mb-[14px] mt-[53px]'>
+         <Heading as='h1' size='lg' className='mb-[14px] mt-[53px]'>
             Sign up
          </Heading>
-         <Heading as='h2' className='text-base font-extrabold mb-6'>
+         <Heading as='h2' size='md' className='mb-6'>
             단국대학교 총학생회 회원가입
          </Heading>
-         <Heading as='h3' className="text-sm before:content-['●'] flex items-center gap-1">
+         <Heading as='h3' size='xs' className="before:content-['●'] flex items-center gap-1">
             이용약관동의
          </Heading>
-         <div className='flex flex-col gap-6'>
-            <Checkbox
-               name='AllAgree'
-               checked={agreeCheck[0] && agreeCheck[1] && agreeCheck[2]}
-               onChange={onAllCheckboxChange}
-               className='ml-auto'
-            >
-               <Text className='text-[10px] font-semibold'>전체동의하기</Text>
-            </Checkbox>
-            <Box>
-               <Checkbox name='agree1' checked={agreeCheck[0]} onChange={onCheckboxChange}>
-                  <Text as='span' className='text-[11px]'>
-                     [필수]
-                  </Text>
-                  <Text as='span' className='text-[11px] text-gray02'>
-                     개인 정보 수집, 이용 동의
-                  </Text>
-               </Checkbox>
-            </Box>
-            <Box>
-               <Checkbox name='agree2' checked={agreeCheck[1]} onChange={onCheckboxChange}>
-                  <Text as='span' className='text-[11px]'>
-                     [필수]
-                  </Text>
-                  <Text as='span' className='text-[11px] text-gray02'>
-                     개인 정보 수집, 이용 동의
-                  </Text>
-               </Checkbox>
-            </Box>
-            <Box className='mb-8'>
-               <Checkbox name='agree3' checked={agreeCheck[2]} onChange={onCheckboxChange}>
-                  <Text as='span' className='text-[11px]'>
-                     [필수]
-                  </Text>
-                  <Text as='span' className='text-[11px] text-gray02'>
-                     개인 정보 수집, 이용 동의
-                  </Text>
-               </Checkbox>
-            </Box>
-         </div>
-         <Button onClick={goNext} className='rounded-2xl w-full py-3'>
-            <Text className='text-base font-bold'>동의 완료</Text>
-         </Button>
+         <Term />
       </div>
    );
 }
--- src/pages/signup/verify.tsx
+++ src/pages/signup/verify.tsx
@@ -1,73 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CONSTANTS, API_PATH } from 'constants/api';
-import { ROUTES } from 'constants/route';
-import axios from 'axios';
-import { Regex } from 'utils/regex';
-import Input from 'components/ui/input';
-import { useAlert } from 'hooks/useAlert';
-import Button from 'components/ui/button';
+import React from 'react';
 import { useEffectOnce } from 'hooks/useEffectOnce';
 import { useLayout } from 'hooks/useLayout';
-import Text from 'components/ui/typo/text';
 import Heading from 'components/ui/typo/heading';
-
-interface StudentVerifyResponse {
-   signupToken: string;
-   student: {
-      studentName: string;
-      studentId: string;
-      major: string;
-   };
-}
-
-interface IVerifyInfo {
-   dkuStudentId: string;
-   dkuPassword: string;
-}
+import VerifyForm from 'components/signup/verify';
 
 export default function SignupVerify() {
-   const navigate = useNavigate();
    const { setLayout } = useLayout();
-
-   const [verifyInfo, setVerifyInfo] = useState<IVerifyInfo>({
-      dkuStudentId: '',
-      dkuPassword: '',
-   });
-
-   const [isFormValid, setIsFormValid] = useState(false);
-
-   const { alert } = useAlert();
-
-   const verify = async (verifyInfo: IVerifyInfo) => {
-      try {
-         const { data } = await axios.post<StudentVerifyResponse>(
-            CONSTANTS.SERVER_URL + API_PATH.USER.SIGNUP.VERIFY,
-            verifyInfo,
-         );
-         navigate(ROUTES.SIGNUP.INFO, { state: { data } });
-      } catch (error) {
-         alert(error);
-      }
-   };
-
-   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = e.target;
-      setVerifyInfo({
-         ...verifyInfo,
-         [name]: value,
-      });
-   };
-
-   useEffect(() => {
-      const isStudentIdValid = Regex.studentId.test(verifyInfo.dkuStudentId);
-      const isPasswordValid = verifyInfo.dkuPassword !== '';
-      setIsFormValid(isStudentIdValid && isPasswordValid);
-   }, [verifyInfo]);
-
-   const handleSubmit = () => {
-      verify(verifyInfo);
-   };
 
    useEffectOnce(() => {
       setLayout({
@@ -75,7 +13,6 @@ export default function SignupVerify() {
          backButton: true,
          isMain: false,
          fullscreen: true,
-         headingStyle: '',
          margin: 'mt-[140px]',
          rounded: true,
       });
@@ -83,48 +20,16 @@ export default function SignupVerify() {
 
    return (
       <div className='flex flex-col px-10 pt-12'>
-         <Heading as='h1' className='text-2xl font-extrabold mb-[14px]'>
+         <Heading as='h1' size='lg' className='mb-[14px]'>
             Sign up
          </Heading>
-         <Heading as='h2' className='text-base font-extrabold mb-6'>
+         <Heading as='h2' size='md' className='mb-6'>
             단국대학교 총학생회 회원가입
          </Heading>
-         <Heading as='h3' className="text-sm before:content-['●'] flex items-center gap-1 mb-8">
+         <Heading as='h3' size='xs' className="text-sm before:content-['●'] flex items-center gap-1 mb-8">
             학생 인증
          </Heading>
-         <form onSubmit={handleSubmit} className='flex flex-col gap-2 max-w-md mx-auto'>
-            <Input
-               type='number'
-               name='dkuStudentId'
-               placeholder='학번 입력'
-               value={verifyInfo.dkuStudentId}
-               onChange={handleInputChange}
-               size='md'
-               className='rounded-[10px]'
-            />
-            <Input
-               type='password'
-               name='dkuPassword'
-               placeholder='비밀번호 입력'
-               value={verifyInfo.dkuPassword}
-               onChange={handleInputChange}
-               size='md'
-               className='rounded-[10px]'
-            />
-            <Text className="mb-8 text-gray02 text-[10.5px] whitespace-pre-wrap mt-2 before:content-['●'] before:mr-1">
-               {
-                  '단국대학교 웹정보 로그인 시 사용 되는 ID, PW를 통해\n 학생인증이 진행됩니다. (입력한 정보는 인증 후 즉시 폐기됩니다)'
-               }
-            </Text>
-            <Button
-               variant='default'
-               className='w-[311px] rounded-2xl py-3'
-               isDisabled={!isFormValid}
-               onClick={handleSubmit}
-            >
-               <Text className='text-base font-bold'>인증</Text>
-            </Button>
-         </form>
+         <VerifyForm />
       </div>
    );
 }
