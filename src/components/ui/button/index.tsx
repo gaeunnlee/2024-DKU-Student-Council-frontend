423a63b186ee50fb7e1264c14674881d1f55a26c
sy-paik <sypaik.dev@gmail.com>
2024-02-28 08:41:49 +0900

    chore: 버튼 variant, size에 따른 커스텀 추가

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Button as ChakraButton } from '@chakra-ui/react';
+import Text from '../typo/text';
 import { ButtonProps as ChakraButtonProps } from '@chakra-ui/react';
 
 interface ButtonProps extends ChakraButtonProps {
-   variant?: 'default' | 'naked';
-   children: React.ReactNode;
+   size: 'xl' | 'lg' | 'md';
+   variant?: 'default' | 'white';
+   children: string;
    className?: string;
    onClick?: () => void;
 }
 
-export default function Button({ variant, className, children, onClick }: ButtonProps) {
-   const buttonVariant = variant === 'default' ? 'bg-black' : 'bg-gray01';
+export default function Button({ size, variant, className, children, onClick, ...props }: ButtonProps) {
+   const buttonVariant = variant === 'default' ? 'black' : 'gray01';
+   const textColor = variant === 'default' ? 'white' : 'black';
+   const buttonSize = {
+      xl: '349px',
+      lg: '336px',
+      md: '316px',
+   };
    return (
-      <ChakraButton className={`bg-black text-white ${className} ${buttonVariant}`} onClick={onClick}>
-         {children}
+      <ChakraButton
+         bg={`${buttonVariant}`}
+         _focus={{ bg: 'current', textColor: 'black' }}
+         _hover={{ bg: 'current' }}
+         width={buttonSize[size]}
+         className={`${className}`}
+         onClick={onClick}
+         {...props}
+      >
+         <Text textColor={textColor}>{children}</Text>
       </ChakraButton>
    );
 }
