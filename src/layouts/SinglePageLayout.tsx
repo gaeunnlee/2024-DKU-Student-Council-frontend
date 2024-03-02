95aedd096efa5321fdf4d534e8eefe37857d36c3
gaeunnlee <gaeunnnleee@gmail.com>
2024-03-03 04:43:25 +0900

    chore: 인사말 페이지 디자인 적용

---
+++ src/layouts/SinglePageLayout.tsx
@@ -0,0 +1,10 @@
+import React, { ComponentProps } from 'react';
+import { IWithReactChildren } from 'shared/interfaces/default-interfaces';
+
+export default function SinglePageLayout({ children, ...props }: IWithReactChildren & ComponentProps<'div'>) {
+   return (
+      <div className='mt-3 p-3 flex flex-col gap-5' {...props}>
+         {children}
+      </div>
+   );
+}
