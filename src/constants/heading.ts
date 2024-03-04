e2585ac3ce5f263bb43bf1293c458543cd0eb134
sy-paik <sypaik.dev@gmail.com>
2024-03-05 01:57:33 +0900

    refactor: heading의 텍스트 및 스타일 상수명 변경

--- src/constants/heading.ts
+++ src/constants/heading.ts
@@ -1,19 +1,57 @@
-export const HeadingStyle = {
-   main: {
-      headingStyle: 'mt-[41px] mb-[3px] text-center',
-      subHeadingStyle: 'text-center font-normal text-[11px] mb-[61px]',
+export const HEADING_TEXT = {
+   MAIN: {
+      HEAD: 'DANKOOK UNIVERSITY',
+      SUBHEAD: 'DANKOOK UNIV STUDENT COUNCIL',
    },
-   default: {
-      header: 'mt-4 py-5 px-7',
-      heading: 'text-xl mb-1',
-      subHeading: 'font-semibold',
+   LOGIN: {
+      HEAD: 'Login',
+      SUBHEAD: '단국대학교 총학생회 로그인',
    },
-   login: {
-      headingStyle: 'mt-7 mb-[19px] text-center',
-      subHeadingStyle: 'text-center mb-[51px] font-extrabold',
+   COUNCIL: {
+      HEAD: '총학생회',
    },
-   reset: {
-      headingStyle: 'mt-[15px] mb-[19px] text-center',
-      subHeadingStyle: 'text-center mb-[55px] font-extrabold',
+   CONFERENCE: {
+      SUBHEAD: '회의록',
+   },
+   NOTICE: {
+      SUBHEAD: '공지',
+   },
+   PETITION: {
+      HEAD: '청원게시판',
+   },
+   RESET_ID_PW: {
+      SUBHEAD: 'ID찾기 PW 재설정',
+   },
+   RULE: {
+      SUBHEAD: '회칙',
+   },
+   ORGANIZATION: {
+      SUBHEAD: '조직도',
+   },
+   LOCATION: {
+      SUBHEAD: '오시는 길',
+   },
+   RECRUIT: {
+      HEAD: '부원모집',
+      SUBHEAD: '모집요강',
+   },
+};
+
+export const HEADING_STYLE = {
+   MAIN: {
+      HEAD: 'mt-[41px] mb-[3px] text-center',
+      SUBHEAD: 'text-center font-normal text-[11px] mb-[61px]',
+   },
+   LOGIN: {
+      HEAD: 'mt-7 mb-[19px] text-center',
+      SUBHEAD: 'text-center mb-[51px] font-extrabold',
+   },
+   COUNCIL: {
+      HEAD: 'ml-[29px] mt-[38px] mb-2',
+      SUBHEAD: 'text-xl ml-[29px] mb-[30px] font-extrabold',
+   },
+   RESET: {
+      HEAD: 'mt-[15px] mb-[19px] text-center',
+      SUBHEAD: 'text-center mb-[55px] font-extrabold',
    },
 };
