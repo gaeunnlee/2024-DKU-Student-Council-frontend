f4a2276ec3f18ab1e928494645d71f92df32bf69
sy-paik <sypaik.dev@gmail.com>
2024-03-05 02:13:20 +0900

    refactor: 임의 텍스트 하이퍼링크 추가

--- src/components/main/service.tsx
+++ src/components/main/service.tsx
@@ -1,14 +1,21 @@
-import Text from 'components/ui/typo/text';
+// import Text from 'components/ui/typo/text';
 import React from 'react';
 
 export default function Service() {
-   //TODO) ios,
+   //TODO) ios, android 하이퍼링크 적용
    return (
       <section className='px-4 py-5 m-4'>
          <h3 className='font-bold text-lg ml-4 mb-4'>With-dankook</h3>
-         <Text as='a' href='https://www.naver.com' rel='noreferrer'>
-            www.naver.com
-         </Text>
+         {/* <div className="bg-white rounded-lg px-2 mb-2">
+            <Text as='a' href='http://localhost:3000/login' rel='noreferrer'>
+            ios
+            </Text>
+         </div>
+         <div className="bg-white rounded-lg px-2">
+            <Text as='a' href='http://localhost:3000/login' rel='noreferrer'>
+            android
+            </Text>
+         </div> */}
       </section>
    );
 }
