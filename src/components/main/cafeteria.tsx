772346c0260234c5519d046c44035cf96ef0f4f9
sy-paik <sypaik.dev@gmail.com>
2024-03-05 02:00:25 +0900

    refactor: cafeteria 쿼리 훅 적용

--- src/components/main/cafeteria.tsx
+++ src/components/main/cafeteria.tsx
@@ -1,48 +1,26 @@
 import React, { useState } from 'react';
-import { useEffectOnce } from 'hooks/useEffectOnce';
-import { useApi } from 'hooks/useApi';
-import { API_PATH } from 'constants/api';
+import { useGetCafeteria } from 'hooks/query/main/query';
 import SvgIcon from 'components/common/icon/SvgIcon';
 
-interface ICafeteria {
-   mealData: string;
-   breakfast: string;
-   lunch: string;
-   dinner: string;
-   [key: string]: string;
-}
-
-const mealOptions = [
-   { key: 'breakfast', icon: 'lunch', label: '아침' },
-   { key: 'lunch', icon: 'lunch', label: '점심' },
-   { key: 'dinner', icon: 'dinner', label: '저녁' },
-];
-
 export default function Cafeteria() {
-   const { get } = useApi();
-   const [cafeteria, setCafeteria] = useState<ICafeteria | null>(null);
    const [selectedMeal, setSelectedMeal] = useState('breakfast');
 
-   const fetchMeal = async () => {
-      const data = await get<ICafeteria>(API_PATH.MAIN.CAFETERIA, {
-         authenticate: true,
-         contentType: 'application/json',
-         log: true,
-      });
-      setCafeteria(data);
-   };
-
-   useEffectOnce(() => {
-      fetchMeal();
-   });
-
-   const handleMealData = (): JSX.Element | null => {
+   const { data: cafeteria } = useGetCafeteria();
+   const handleMealData = () => {
       return cafeteria ? <p>{cafeteria[selectedMeal]}</p> : null;
    };
+   const mealButton =
+      'flex items-center gap-[2px] w-[70px] h-6 text-center text-sm px-3 rounded-xl shadow-md bg-no-repeat';
+
+   const mealOptions = [
+      { key: 'breakfast', icon: 'lunch', label: '아침' },
+      { key: 'lunch', icon: 'lunch', label: '점심' },
+      { key: 'dinner', icon: 'dinner', label: '저녁' },
+   ];
 
    return (
       <section className='px-4 py-5 m-4 rounded-xl bg-white shadow-md'>
-         <h4 className='font-bold text-lg ml-4 mb-4'>오늘의 학식</h4>
+         <h4 className='font-bold text-lg mb-4'>오늘의 학식</h4>
          <div className='flex gap-2'>
             <ul className='flex flex-col gap-1'>
                {mealOptions.map((option) => (
@@ -50,9 +28,7 @@ export default function Cafeteria() {
                      <button
                         className={`${
                            selectedMeal === option.key ? 'bg-black text-white' : 'bg-white text-black'
-                        } flex items-center gap-[2px] w-[70px] h-6 text-center text-sm px-3 rounded-xl bg-${
-                           option.key
-                        } shadow-md bg-no-repeat`}
+                        } ${mealButton}`}
                         onClick={() => setSelectedMeal(option.key)}
                      >
                         <SvgIcon
@@ -66,7 +42,7 @@ export default function Cafeteria() {
                   </li>
                ))}
             </ul>
-            <div className='w-[250px] px-4 py-5'>{handleMealData()}</div>
+            <div className='w-[250px] px-4 py-5 text-sm'>{handleMealData()}</div>
          </div>
       </section>
    );
