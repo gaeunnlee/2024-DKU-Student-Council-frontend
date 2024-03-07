430600b1e4574e786b05f3b7272d8c9cd623cede
sy-paik <sypaik.dev@gmail.com>
2024-03-07 17:18:33 +0900

    refactor: className props 추가

--- src/components/common/icon/SvgIcon.tsx
+++ src/components/common/icon/SvgIcon.tsx
@@ -1,17 +1,18 @@
 import IconSprite from 'assets/icons/IconSprite.svg';
 import React from 'react';
 
-interface IconProps {
+export interface IconProps {
    id: string;
    color?: string;
    width: number;
    height: number;
+   className?: string;
 }
 
-const SvgIcon = ({ id, width, height, color }: IconProps) => {
+const SvgIcon = ({ id, width, height, color, className }: IconProps) => {
    return (
       <svg width={width} height={height} fill={color}>
-         <use href={`${IconSprite}#${id}`} className={`text-${color}`} />
+         <use href={`${IconSprite}#${id}`} className={`text-${color} ${className ?? ''}`} />
       </svg>
    );
 };
