2b21dc253fe791abdad7e6e5ef35637228497bdf
sy-paik <sypaik.dev@gmail.com>
2024-03-07 17:19:25 +0900

    feat: IconButton 컴포넌트 추가

---
+++ src/components/ui/button/IconButton.tsx
@@ -0,0 +1,15 @@
+import SvgIcon from 'components/common/icon/SvgIcon';
+import { IconProps } from 'components/common/icon/SvgIcon';
+import React from 'react';
+
+interface IconButtonProps extends IconProps {
+   onClick: () => void;
+}
+
+export default function IconButton({ onClick, ...props }: IconButtonProps) {
+   return (
+      <button onClick={onClick}>
+         <SvgIcon {...props} />
+      </button>
+   );
+}
