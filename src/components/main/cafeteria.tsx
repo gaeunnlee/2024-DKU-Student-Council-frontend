92fc7dd7a81803edb11ac2d42adf642a53c22946
sy-paik <sypaik.dev@gmail.com>
2024-03-07 17:29:26 +0900

    refactor: 학식 데이터가 없을 경우 emptyCafeteria 반환하도록 수정

--- src/components/main/cafeteria.tsx
+++ src/components/main/cafeteria.tsx
@@ -5,10 +5,13 @@ import SvgIcon from 'components/common/icon/SvgIcon';
 export default function Cafeteria() {
    const [selectedMeal, setSelectedMeal] = useState('breakfast');
 
+   const emptyCafeteria = '학식 데이터가 존재하지 않습니다.';
+
    const { data: cafeteria } = useGetCafeteria();
    const handleMealData = () => {
-      return cafeteria ? <p className='whitespace-pre-line'>{cafeteria[selectedMeal]}</p> : null;
+      return <p className='whitespace-pre-line'>{cafeteria[selectedMeal] ?? emptyCafeteria}</p>;
    };
+
    const mealButton =
       'flex items-center gap-[2px] w-[70px] h-6 text-center text-sm px-3 rounded-xl shadow-md bg-no-repeat';
 
