fe3a14cf26dfc1fcbd15c1d1fed7ca2e676f9ac5
sy-paik <sypaik.dev@gmail.com>
2024-03-14 01:52:56 +0900

    chore: import 순서 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,8 +25,26 @@ module.exports = {
       ecmaVersion: 'latest',
       sourceType: 'module',
    },
-   plugins: ['@typescript-eslint', 'react', 'cypress'],
+   plugins: ['@typescript-eslint', 'react', 'cypress', 'import'],
    rules: {
+      'import/order': [
+         'error',
+         {
+            groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+            pathGroups: [
+               {
+                  pattern: 'angular',
+                  group: 'external',
+                  position: 'before',
+               },
+            ],
+            alphabetize: {
+               order: 'asc',
+               caseInsensitive: true,
+            },
+            'newlines-between': 'always',
+         },
+      ],
       indent: ['error', 3, { SwitchCase: 1 }],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
