1bd3a67bb755024a7315c96100bc43a98e57463d
sy-paik <sypaik.dev@gmail.com>
2024-03-14 21:14:35 +0900

    style: 메시지 텍스트 스타일 css 추가

--- src/components/ui/typo/message.tsx
+++ src/components/ui/typo/message.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Message({ children }: { children: React.ReactNode }) {
-   return <p>{children}</p>;
+   return <p className='text-[11px] text-center'>{children}</p>;
 }
