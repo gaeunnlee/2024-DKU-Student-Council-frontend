6e7716902c472e6119aa024cb5752f34cfa32111
sy-paik <sypaik.dev@gmail.com>
2024-03-14 21:13:05 +0900

    fix: verify mutate 안되는 오류 수정

--- src/components/signup/verify.tsx
+++ src/components/signup/verify.tsx
@@ -15,6 +15,10 @@ export default function VerifyForm() {
 
    const { mutate } = usePostStudentVerify();
 
+   const handleVerifySubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      mutate(verifyInfo);
+   };
    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const { name, value } = e.target;
       setVerifyInfo({
@@ -30,8 +34,8 @@ export default function VerifyForm() {
    }, [verifyInfo]);
 
    return (
-      <>
-         <form className='flex flex-col gap-2 max-w-md mx-auto'>
+      <React.Fragment>
+         <form onSubmit={handleVerifySubmit} className='flex flex-col gap-2 max-w-md mx-auto'>
             <Input
                type='number'
                name='dkuStudentId'
@@ -55,16 +59,10 @@ export default function VerifyForm() {
                   '단국대학교 웹정보 로그인 시 사용 되는 ID, PW를 통해\n 학생인증이 진행됩니다. (입력한 정보는 인증 후 즉시 폐기됩니다)'
                }
             </p>
-            <Button
-               size='md'
-               variant='default'
-               className='rounded-[20px]'
-               disabled={!isFormValid}
-               onClick={() => mutate(verifyInfo)}
-            >
+            <Button size='md' variant='default' className='rounded-[20px]' disabled={!isFormValid}>
                인증
             </Button>
          </form>
-      </>
+      </React.Fragment>
    );
 }
