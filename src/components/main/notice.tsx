80cdee4bf87ef262f47fe9a63d4bd2e1efe3b5f7
sy-paik <sypaik.dev@gmail.com>
2024-03-24 00:23:17 +0900

    refactor: skeleton 컴포넌트 삭제

--- src/components/main/notice.tsx
+++ src/components/main/notice.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@components/ui/button';
-import { BaseSkeleton, TextSkeleton } from '@components/ui/skeleton';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -18,7 +17,7 @@ export default function Notice({ notices }: { notices?: INotice[] }) {
       navigate('/notice');
    };
 
-   return notices ? (
+   return (
       <section className='px-4 py-5 m-4 rounded-xl bg-white shadow-md'>
          <div className='flex items-center justify-between'>
             <h3 className='font-bold text-lg'>총학소식</h3>
@@ -34,16 +33,5 @@ export default function Notice({ notices }: { notices?: INotice[] }) {
             ))}
          </ul>
       </section>
-   ) : (
-      <BaseSkeleton className='px-4 py-5 m-4 rounded-xl'>
-         <h3 className='font-bold text-lg'>총학소식</h3>
-         <ul className='mt-3 flex flex-col gap-2'>
-            <TextSkeleton className='my-3 rounded-full' width={7} />
-            <TextSkeleton className='my-3 rounded-full' width={15} />
-            <TextSkeleton className='my-3 rounded-full' width={10} />
-            <TextSkeleton className='my-3 rounded-full' width={17} />
-            <TextSkeleton className='my-3 rounded-full' width={14} />
-         </ul>
-      </BaseSkeleton>
    );
 }
