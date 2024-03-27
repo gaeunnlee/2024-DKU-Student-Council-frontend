7c7951c4ee239402d9d9b03e25bcd7c15531e9ea
sy-paik <sypaik.dev@gmail.com>
2024-03-27 18:35:43 +0900

    refactor: form 업로드 handleSubmit 분리

--- src/pages/notice/post.tsx
+++ src/pages/notice/post.tsx
@@ -1,30 +1,39 @@
-import { IFormInfo } from '@api/upload/types/upload';
 import Post from '@components/main/post';
-import { API_PATH } from '@constants/api';
-import { ROUTES } from '@constants/route';
+import { usePostNoticeForm } from '@hooks/api/notice/usePostNoticeForm';
+import { PostFormInfo } from '@hooks/api/notice/usePostNoticeForm';
 import { useFormUpload } from '@hooks/useFormUpload';
 import useImageUpload from '@hooks/useImageUpload';
 import React from 'react';
 
 export default function NoticePost() {
-   const initFormInfo: IFormInfo = {
+   const initFormInfo: PostFormInfo = {
       title: '',
       body: '',
       files: [],
    };
 
-   const { formInfo, setFormInfo, handleUpdate, handleSubmit } = useFormUpload({
-      initFormInfo: initFormInfo,
-      API_PATH: API_PATH.POST.NOTICE.ROOT,
-      NAVIGATE_PATH: ROUTES.NOTICE.ROOT,
-   });
+   const { formInfo, setFormInfo, handleUpdate } = useFormUpload(initFormInfo);
+
+   const { mutate } = usePostNoticeForm();
+
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append('title', formInfo.title);
+      formData.append('body', formInfo.body);
+      if (formInfo.files !== undefined) {
+         for (const file of formInfo.files) {
+            formData.append('files', file);
+         }
+      }
+      mutate(formInfo);
+   };
 
    const { imageList, addImage, deleteImage } = useImageUpload();
 
    return (
       <Post
-         pageTitle='공지'
-         navigateUrl={ROUTES.NOTICE.POST}
+         postType='공지'
          formInfo={formInfo}
          setFormInfo={setFormInfo}
          handleUpdate={handleUpdate}
