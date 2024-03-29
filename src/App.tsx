b8e1e2ed4536480913472c5f86cf929aa7fedff4
sy-paik <sypaik.dev@gmail.com>
2024-03-29 23:29:26 +0900

    refactor: createbrowserrouter에서 기존 router로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-// import ErrorBoundary from '@components/common/errorBoundary';
-// import Error from '@components/errorFallback';
 import ModalProvider from '@components/ui/modal/modal-provider';
-// import { useResetError } from '@hooks/useResetErrorBoundary';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Router from './Router';
 
 const queryClient = new QueryClient({
    defaultOptions: {
       queries: {
-         retry: 1,
+         retry: 0,
+         refetchOnWindowFocus: false,
+         throwOnError: true,
       },
    },
 });
 
 export default function App() {
-   // const { handleErrorReset } = useResetError();
-
    return (
       <QueryClientProvider client={queryClient}>
          <ModalProvider>
-            {/* <ErrorBoundary Fallback={Error} onReset={handleErrorReset}> */}
-            <Suspense>
-               <Outlet />
-            </Suspense>
-            {/* </ErrorBoundary> */}
+            <BrowserRouter>
+               <Router />
+            </BrowserRouter>
          </ModalProvider>
       </QueryClientProvider>
    );
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
 
-import Router from './Router';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<RouterProvider router={Router} />);
+root.render(<App />);
