75e521da5fe9d5ebaf29b145f61b2b9cde32fb96
sy-paik <sypaik.dev@gmail.com>
2024-04-05 04:18:23 +0900

    refactor: 불필요한 useFetchMyInfo 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
 import Carousel from '@components/common/carousel';
-import { Notice, Petition, Cafeteria, AppDownload } from '@components/main/';
+import { Cafeteria, AppDownload, Notice, Petition } from '@components/main/';
 import NoticeSkeleton from '@components/ui/skeleton/main';
 import { Spinner } from '@components/ui/spinner/indext';
 import { HEADING_TEXT, HEADING_STYLE } from '@constants/heading';
 import useGetMain from '@hooks/api/main/useGetMain';
 import { useEffectOnce } from '@hooks/useEffectOnce';
-// import { useFetchMyInfo } from '@hooks/useFetchMyInfo';
 import { useLayout } from '@hooks/useLayout';
 import React, { Suspense } from 'react';
 
+
+
 export default function Main() {
    const { setLayout } = useLayout();
-   // const { fetchMyInfo } = useFetchMyInfo();
    const { data: main } = useGetMain();
-
+   
    useEffectOnce(() => {
-      // fetchMyInfo();
       setLayout({
          title: null,
          backButton: false,
@@ -28,7 +27,7 @@ export default function Main() {
          rounded: false,
       });
    });
-
+   //TODO) Cafeteria skeleton 추가
    return (
       <React.Fragment>
          <div className='w-full h-[337px] flex justify-center'>
@@ -46,7 +45,9 @@ export default function Main() {
             <Suspense fallback={<NoticeSkeleton />}>
                <Petition petitions={main?.popularPetitions} />
             </Suspense>
-            <Cafeteria />
+            <Suspense fallback={<div>로딩중</div>}>
+               <Cafeteria />
+            </Suspense>
             <AppDownload />
          </div>
       </React.Fragment>
