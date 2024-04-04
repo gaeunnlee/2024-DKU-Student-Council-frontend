29a2fb3658bda57d21f068037b967decc12cc8ab
sy-paik <sypaik.dev@gmail.com>
2024-04-05 04:17:34 +0900

    refactor: response 타입 추가 및 response.data로 반환

--- src/hooks/api/signup/useGetNicknameVerify.ts
+++ src/hooks/api/signup/useGetNicknameVerify.ts
@@ -1,13 +1,19 @@
 import { API_PATH } from '@constants/api';
 import { get } from '@libs/api';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 
-export const useGetNicknameVerify = (nickname: string) => {
-   return useQuery({
+interface NicknameVerifyResponse {
+   message: string;
+}
+
+export const useGetNicknameVerify = (nickname: string, options?: UseQueryOptions<NicknameVerifyResponse>) => {
+   return useQuery<NicknameVerifyResponse>({
       queryKey: ['nickname'],
       queryFn: () => {
          return get(API_PATH.USER.SIGNUP.INFO.NICKNAME, { params: { nickname } });
       },
+      select: (data) => data,
       enabled: false,
+      ...options,
    });
 };
