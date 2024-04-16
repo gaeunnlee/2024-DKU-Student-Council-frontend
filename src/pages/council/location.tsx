54056325396a4b00b9090ff4ead85191b014f5d0
sy-paik <sypaik.dev@gmail.com>
2024-04-16 23:54:34 +0900

    refactor: council 페이지 전체 레이아웃 수정 * Gnb, HeaderSection, ContentSection 사용

--- src/pages/council/location.tsx
+++ src/pages/council/location.tsx
@@ -1,53 +1,60 @@
 import map from '@assets/images/map.png';
+import { Gnb, GnbGoBack, GnbTitle } from '@components/common/gnb';
+import { GnhTitle } from '@components/common/gnh';
+import { ContentSection, HeaderSection } from '@components/layouts';
 import SinglePageLayout from '@components/layouts/SinglePageLayout';
-import { HEADING_TEXT, HEADING_STYLE } from '@constants/heading';
-import { useEffectOnce } from '@hooks/useEffectOnce';
-import { useLayout } from '@hooks/useLayout';
+import Selector from '@components/ui/selector';
+import { HEADING_TEXT, COUNCIL_LIST } from '@constants/heading';
 import React from 'react';
 
-export default function Location() {
-   const { setLayout } = useLayout();
 
-   useEffectOnce(() => {
-      setLayout({
-         title: HEADING_TEXT.COUNCIL.HEAD,
-         backButton: true,
-         isMain: false,
-         fullscreen: false,
-         headingText: HEADING_TEXT.COUNCIL.HEAD,
-         subHeadingText: HEADING_TEXT.LOCATION.SUBHEAD,
-         headingStyle: HEADING_STYLE.COUNCIL.HEAD,
-         subHeadingStyle: HEADING_STYLE.COUNCIL.SUBHEAD,
-         rounded: true,
-         dropDown: HEADING_STYLE.COUNCIL.DROPDOWN,
-      });
-   });
+export default function Location() {
+   const LOCATION_INFO = [
+      {
+         KEY: '위치',
+         VALUE: '해당관 406호 총학생회실'
+      },
+      {
+         KEY: '주소',
+         VALUE: '(16890) 경기도 용인시 수지구 죽전동 1491 단국대학교 혜당관 406호 총학생회실',
+      },
+      {
+         KEY: '전화',
+         VALUE: '031)8005-2680-1',
+      },
+      {
+         KEY: '이메일',
+         VALUE: 'dkudamda@gmail.com',
+      },
+      {
+         KEY: '인스타그램',
+         VALUE: '@dku_damda',
+      },
+   ];
 
    return (
-      <SinglePageLayout>
-         <img className='w-[366px] h-[240px] mx-auto' src={map} alt='단국대학교 총학생회 지도' />
-         <ul className='flex flex-col gap-4'>
-            <li>
-               <h2 className='font-semibold'>위치</h2>
-               <span>혜당관 406호 총학생회실</span>
-            </li>
-            <li>
-               <h2 className='font-semibold'>주소</h2>
-               <span>(16890) 경기도 용인시 수지구 죽전동 1491 단국대학교 혜당관 406호 총학생회실</span>
-            </li>
-            <li>
-               <h2 className='font-semibold'>전화</h2>
-               <span>{'031)'}8005-2680-1</span>
-            </li>
-            <li>
-               <h2 className='font-semibold'>이메일</h2>
-               <span>dkudamda@gmail.com</span>
-            </li>
-            <li>
-               <h2 className='font-semibold'>인스타그램</h2>
-               <span>@dku_damda</span>
-            </li>
-         </ul>
-      </SinglePageLayout>
+      <React.Fragment>
+         <Gnb>
+            <GnbGoBack />
+            <GnbTitle>{HEADING_TEXT.COUNCIL.HEAD}</GnbTitle>
+         </Gnb>
+         <HeaderSection className="pt-[38px] pl-[29px] pb-[30px]">
+            <GnhTitle>{HEADING_TEXT.COUNCIL.HEAD}</GnhTitle>
+            <Selector list={COUNCIL_LIST} subHeadingText={HEADING_TEXT.LOCATION.SUBHEAD} />
+         </HeaderSection>
+         <ContentSection showNav={true}>
+            <SinglePageLayout>
+               <img className='w-[366px] h-[240px] mx-auto' src={map} alt='단국대학교 총학생회 지도' />
+               <ul className='flex flex-col gap-4'>
+                  {LOCATION_INFO.map((info, index) => (
+                     <li key={index}>
+                        <h2 className="font-semibold">{info.KEY}</h2>
+                        <span>{info.VALUE}</span>
+                     </li>
+                  ))}
+               </ul>
+            </SinglePageLayout>
+         </ContentSection>
+      </React.Fragment>
    );
 }
