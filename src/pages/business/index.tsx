639c734f18c28ee9bb6213bb4c3f549fe9a8ec1c
sy-paik <sypaik.dev@gmail.com>
2024-04-17 00:06:06 +0900

    refactor: 제휴 페이지 전체 레이아웃 수정

--- src/pages/business/index.tsx
+++ src/pages/business/index.tsx
@@ -1,22 +1,26 @@
+import { Gnb, GnbGoBack, GnbTitle } from '@components/common/gnb';
+import { GnhTitle } from '@components/common/gnh';
+import { ContentSection, HeaderSection } from '@components/layouts';
 import Board from '@components/ui/board';
 import IntersectionBox from '@components/ui/box/intersectionBox';
 import ItemList from '@components/ui/item-list';
+import Selector from '@components/ui/selector';
 import { Spinner } from '@components/ui/spinner/indext';
-import { HEADING_TEXT, HEADING_STYLE } from '@constants/heading';
+import { HEADING_TEXT, BUSINESS_LIST } from '@constants/heading';
+import { ROUTES } from '@constants/route';
 import { useGetCoalitionList } from '@hooks/api/coalition/useGetCoalitionList';
 import { CoalitionContentResponse } from '@hooks/api/coalition/useGetCoalitionList';
 import { useInfiniteScroll } from '@hooks/useInfiniteScroll';
-import { useLayout } from '@hooks/useLayout';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
-import { ROUTES } from '@/constants/route';
+
 import { CoalitionType } from '@/types/coalition';
+
 
 export default function BusinessBoard() {
    const category = useParams();
-   const { setLayout } = useLayout();
    const navigate = useNavigate();
    const categoryType = category.category?.toUpperCase();
 
@@ -32,23 +36,6 @@ export default function BusinessBoard() {
       refetch();
    }, [category, refetch]);
 
-   useEffect(() => {
-      setLayout({
-         title: HEADING_TEXT.COUNCIL.HEAD,
-         backButton: true,
-         isMain: false,
-         fullscreen: false,
-         headingText: HEADING_TEXT.BUSINESS.HEAD,
-         subHeadingText:
-            Object.getOwnPropertyDescriptor(HEADING_TEXT.BUSINESS.SUBHEAD, categoryType as CoalitionType)
-               ?.value ?? '',
-         headingStyle: HEADING_STYLE.COUNCIL.HEAD,
-         subHeadingStyle: HEADING_STYLE.COUNCIL.SUBHEAD,
-         rounded: true,
-         dropDown: HEADING_STYLE.BUSINESS.DROPDOWN,
-      });
-   }, [category]);
-
    const goToBusinessDetail = (item: CoalitionContentResponse) => {
       navigate(ROUTES.BUSINESS.DETAIL(category.category?.toLowerCase() as string, item.id.toString()), {
          state: item,
@@ -56,16 +43,29 @@ export default function BusinessBoard() {
    };
 
    return (
-      <Board>
-         {coalition?.pages.map((page) =>
-            page.content.map((item) => (
-               <Board.Cell key={item.id} onClick={() => goToBusinessDetail(item)}>
-                  <ItemList content={item} />
-               </Board.Cell>
-            )),
-         )}
-         <IntersectionBox ref={intersectionRef} />
-         {isFetchingNextPage && <Spinner />}
-      </Board>
+      <React.Fragment>
+         <Gnb>
+            <GnbGoBack />
+            <GnbTitle>{HEADING_TEXT.COUNCIL.HEAD}</GnbTitle>
+         </Gnb>
+         <HeaderSection className="pt-[38px] pl-[29px] pb-[30px]">
+            <GnhTitle className="mb-2">{HEADING_TEXT.BUSINESS.HEAD}</GnhTitle>
+            <Selector list={BUSINESS_LIST} subHeadingText={Object.getOwnPropertyDescriptor(HEADING_TEXT.BUSINESS.SUBHEAD, categoryType as CoalitionType)
+               ?.value ?? ''} />
+         </HeaderSection>
+         <ContentSection showNav={true}>
+            <Board>
+               {coalition?.pages.map((page) =>
+                  page.content.map((item) => (
+                     <Board.Cell key={item.id} onClick={() => goToBusinessDetail(item)}>
+                        <ItemList content={item} />
+                     </Board.Cell>
+                  )),
+               )}
+               <IntersectionBox ref={intersectionRef} />
+               {isFetchingNextPage && <Spinner />}
+            </Board>
+         </ContentSection>
+      </React.Fragment>
    );
 }
