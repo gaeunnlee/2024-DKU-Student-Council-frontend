9a7e25208c07ad7ac61373e1afd983e72886ae85
sy-paik <sypaik.dev@gmail.com>
2024-04-17 14:48:45 +0900

    style: 불필요한 heading_style 삭제

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,11 +1,9 @@
 import { Palette } from '@constants/colors';
-import { HEADING_STYLE } from '@constants/heading';
 import { bottomNavSize } from '@constants/nav';
 import { TextStyle } from '@constants/textStyle';
 
 export const styles = {
    Palette,
-   HEADING_STYLE,
    TextStyle,
    bottomNavSize,
 };
