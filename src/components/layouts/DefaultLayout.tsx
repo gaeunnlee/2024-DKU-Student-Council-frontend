19047315083268f46794d058e2f383cd1277a71c
sy-paik <sypaik.dev@gmail.com>
2024-04-17 14:35:39 +0900

    feat: menu 컴포넌트 추가

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,8 +1,17 @@
+import Menu from '@components/main/menu';
+import { menuStore } from '@stores/menu-store';
 import React from 'react';
 
 const DefaultLayout = ({ children, className, ...props }: React.ComponentProps<'div'>) => {
+   const { menuOpen } = menuStore();
    return (
-      <div className={`w-[390px] mx-auto bg-black ${className ?? ''}`} {...props}>{children}</div>
+      <div className={`w-[390px] mx-auto bg-black ${className ?? ''}`} {...props}>
+         {menuOpen ? (
+            <Menu />
+         ) : (
+            children
+         )}
+      </div>
    );
 };
 
